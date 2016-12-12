2b91668b2f3b8e841c9e85eff5158dd3d88bf521
Mehran Khajavi <sinoohe.mkh@gmail.com>
2016-12-12 21:31:27 +0330

    create channelWrapper for each request

--- push.js
+++ push.js
@@ -9,14 +9,8 @@ function Push(routeName, connection) {
   var vm = this;
   this.routeName = routeName;
   this.connection = connection;
-  var channelWrapper = vm.connection.createChannel({
-    setup: function (channel) {
-      return channel.assertQueue(vm.routeName, {
-        durable: true
-      });
-    }
-  });
-  vm.channelWrapper = channelWrapper;
+
+
   vm.responseTimeout = 60000; //any timeout limit, IMPORTANT: if you want to change it you should update queues properties in rabbitmq or delete the queues!
   return Q.resolve(vm);
 }
@@ -27,11 +21,20 @@ function Push(routeName, connection) {
  */
 Push.prototype.sendPush = function (data) {
   var vm = this;
-  var channelWrapper = vm.channelWrapper;
+  var channelWrapper = vm.connection.createChannel({
+    setup: function (channel) {
+      return channel.assertQueue(vm.routeName, {
+        durable: true
+      });
+    }
+  });
   try {
     Q(channelWrapper.sendToQueue(vm.routeName, new Buffer(JSON.stringify(data))))
       .catch((err) => {
         console.error('error send messaging', err);
+      })
+      .done(() => {
+        channelWrapper.close();
       });
   } catch (err) {
     console.log('cant send to queue in messaging queue', err);
@@ -112,10 +115,66 @@ Push.prototype._onResponseReceived = function (channel, msg) {
 Push.prototype.rpcCall = function (data) {
   var deferred = Q.defer();
   var vm = this,
-    channelWrapper = vm.channelWrapper;
+    channelWrapper = vm.connection.createChannel({
+      setup: function (channel) {
+        return channel.assertQueue(vm.routeName, {
+          durable: true
+        });
+      }
+    });
   var correlationId = generateUuid(),
-    replyQueue = vm._getReplyName();
-  vm._registerReceiverChannelWrapper();
+    replyQueue = vm._getReplyName(),
+    timeout,
+    receiverChannelWrapper;
+
+  var resolve = function (data) {
+    deferred.resolve(data);
+    channelWrapper.close();
+    receiverChannelWrapper.close();
+  };
+  var reject = function (err) {
+    deferred.reject(err);
+    channelWrapper.close();
+    receiverChannelWrapper.close();
+  };
+
+  receiverChannelWrapper = vm.connection.createChannel({
+    setup: function (channel) {
+      return Q(channel.assertQueue(vm._getReplyName(), {
+        durable: true,
+        messageTtl: vm.responseTimeout
+      })).then(() => {
+        var replyName = vm._getReplyName();
+        return Q(channel.consume(replyName, (msg) => {
+          var corrId = _.get(msg, 'properties.correlationId', false);
+          if (corrId === correlationId) {
+            if (timeout) {
+              clearTimeout(timeout);
+            }
+            channel.ack(msg);
+            var parsed;
+            try {
+              parsed = JSON.parse(msg.content.toString());
+            } catch (e) {
+              console.log('messaging:error parsing rpc call response', {
+                routeName: vm._getReplyName(),
+                error: e,
+                incomingmsg: msg
+              });
+            }
+            if (_.get(parsed, 'success')) {
+              resolve(_.get(parsed, 'result'));
+            } else {
+              reject(_.get(parsed, 'error'));
+            }
+          } else {
+            channel.nack(msg);
+          }
+        }));
+      });
+    }
+  });
+
   //no need to catch the result:
   Q.fcall(() => {
     Q(channelWrapper.sendToQueue(
@@ -128,24 +187,16 @@ Push.prototype.rpcCall = function (data) {
       console.error('amqp: error in connection sendToQueue rpc call');
     }); //no need to return promise, due to error in stability of if not connect
 
-    var timeout = false;
-    if (vm.responseTimeout > 0) {
-      timeout = setTimeout((corrId) => {
-        var entity = _.get(vm.requests, corrId);
-        console.error('timeout occured');
-        entity.deferred.reject('timeout');
-        _.unset(vm.requests, corrId);
-      }, vm.responseTimeout, correlationId);
-    }
-    _.set(vm.requests, correlationId, {
-      deferred: deferred,
-      timeout: timeout
-    });
 
+    if (vm.responseTimeout > 0) {
+      timeout = setTimeout(() => {
+        console.error('timeout occured');
+        deferred.reject('timeout');
+      }, vm.responseTimeout);
+    }
   }).catch((err) => {
     console.error('rpccall send queue fail', err);
-    _.unset(vm.requests, correlationId);
-    deferred.reject(err);
+    reject(err);
   });
 
   return deferred.promise;
--- service.js
+++ service.js
@@ -148,11 +148,11 @@ var pushProviders = {};
  */
 service.prototype.getPushProvider = function (routeName) {
   /*  if (_.has(pushProviders, routeName)) {
-      return _.get(pushProviders, routeName);
-    }
-    var push = new pushProvider(routeName, connection);
-    _.set(pushProviders, routeName, push);
-   return push;*/
+        return _.get(pushProviders, routeName);
+      }
+      var push = new pushProvider(routeName, connection);
+      _.set(pushProviders, routeName, push);
+     return push;*/
   //Due to amqp-connection-manager bug we create it each time, but it SHOULD be fixed
   return new pushProvider(routeName, connection);
 };
